5665670cf9284e3377c1275021e204dbc2b43ef6
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2023-03-01 22:24:39 +0000

    Added file creation

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ func main() {
 		log.Print("GOFILE cannot be blank")
 	}
 
+	if len(os.Args) != 2 {
+		log.Fatal("Must pass the desired output file as the only argument")
+	}
+
+	outFile := os.Args[1]
+
 	log.Printf("Generating Overmind docs for %v", fileName)
 
 	fset := token.NewFileSet()
@@ -29,4 +35,17 @@ func main() {
 		doc.ParseGroup(group)
 	}
 
+	md, err := doc.FormatMarkdown()
+
+	if err != nil {
+		log.Fatalf("Error generating markdown: %v", err)
+	}
+
+	err = os.WriteFile(outFile, []byte(md), 0644)
+
+	if err != nil {
+		log.Fatalf("Error writing file %v: %v", outFile, err)
+	}
+
+	log.Printf("Markdown written: %v", outFile)
 }
